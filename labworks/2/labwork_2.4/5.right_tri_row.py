f8d96b4fdc8f8a8652df420b8bcbafd2872d3f7c
fennie0112 <tatmiyafeni@gmail.com>
2026-09-02 17:27:41 +0530

    commit

--- labworks/2/labwork_2.4/5.right_tri_row.py
+++ labworks/2/labwork_2.4/5.right_tri_row.py
@@ -1,4 +1,5 @@
+
 for row in range(1,6):
     for col in range(1,row+1):
-        print(row,end=" ")
+        print(col,end=" ")
     print()
--- labworks/2/labwork_2.4/7.tri_pattern.py
+++ labworks/2/labwork_2.4/7.tri_pattern.py
@@ -1,4 +1,4 @@
-for i in range(5, 0, -1):
-    for j in range(i,6):
-        print(j, end=" ")
+for row in range(5, 0, -1):
+    for col in range(row, 6):
+        print(col, end=" ")
     print()
